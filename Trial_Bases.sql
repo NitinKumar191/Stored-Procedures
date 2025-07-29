ccf0faae41473b13ed4c3dfdae02355f5193aee2
NitinKumar191 <Nitin.Kumar@equipped.ai>
2025-07-29 15:46:45 +0530

    Added LoanID Check

--- Trial_Bases.sql
+++ Trial_Bases.sql
@@ -5,6 +5,6 @@ AS
 Begin
 
 SELECT * FROM [vw_tblinvestment] WHERE Counterparty = 'Borrower AS' 
-and Currency = 'GBP'
+and Currency = 'GBP' and LoanID = '80af18da-7e9d-4f9d-abcc-66c4649b1c62'
 
 End
